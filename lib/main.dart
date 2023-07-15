5bb93cd7b9b77a9170ab35f28f49789bed78a92e
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-07-15 12:14:54 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:chat_application/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if(kIsWeb){
+    await Firebase.initializeApp(options: FirebaseOptions(
+        apiKey: Constants.apiKey,
+        appId: Constants.appId,
+        messagingSenderId: Constants.messagingSenderId,
+        projectId: Constants.projectId
+    ));
+  }
+  else{
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatNovo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
+
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+class Constants{
+
+  static String appId = "1:321080410087:web:c21f4e3880ce8a5cb3c742";
+  static String apiKey = "AIzaSyA-9JyQiijduwSheKQMMo3Ou1EIFMMAQmU";
+  static String messagingSenderId = "321080410087";
+  static String projectId = "chat-application-5b742";
+
+
+}
